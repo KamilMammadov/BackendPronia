0ca768cfe1f2319a95537f95120413f148dfdf1a
KamilMammadov <kamiltm@code.edu.az>
2023-01-24 21:38:23 +0400

    Payment benefits client panel created

--- DemoApp/DemoApplication/Areas/Client/ViewComponents/Benefit.cs
+++ DemoApp/DemoApplication/Areas/Client/ViewComponents/Benefit.cs
@@ -1,4 +1,7 @@
-﻿using DemoApplication.Database;
+﻿using DemoApplication.Areas.Client.ViewModels;
+using DemoApplication.Contracts.File;
+using DemoApplication.Database;
+using DemoApplication.Services.Abstracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApplication.Areas.Client.ViewComponents
@@ -8,16 +11,20 @@ namespace DemoApplication.Areas.Client.ViewComponents
     public class Benefit:ViewComponent
     {
         private readonly DataContext _dataContext;
+        private readonly IFileService _fileService;
 
-        public Benefit(DataContext dataContext)
+
+        public Benefit(DataContext dataContext,IFileService fileService)
         {
             _dataContext = dataContext;
+            _fileService = fileService;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var model =
-                _dataContext.PaymentBenefits.ToList();
+                _dataContext.PaymentBenefits.Select(b=>new BenefitListItemViewModel(b.Title,b.Content, _fileService
+                 .GetFileUrl(b.ImageInFileSystem, UploadDirectory.PaymentBenefits))).ToList();
 
             return View(model);
         }
