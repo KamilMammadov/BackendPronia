6ae403c95f0cdf6da6744b96466ed7cbf8e6a97d
KamilMammadov <kamiltm@code.edu.az>
2023-01-29 15:57:15 +0400

    CheckoutController Completed

---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/OrdersProductsViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace DemoApplication.Areas.Client.ViewModels
+{
+    public class OrdersProductsViewModel
+    {
+        public List<ItemViewModel>? Products { get; set; }
+        public SummaryViewModel? Summary { get; set; }
+
+
+        public class SummaryViewModel
+        {
+            public decimal Total { get; set; }
+        }
+
+        public class ItemViewModel
+        {
+            public string? Name { get; set; }
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
+            public decimal Total { get; set; }
+            public string ColorName { get; set; }
+            public string SizeName { get; set; }
+        }
+    }
+}
